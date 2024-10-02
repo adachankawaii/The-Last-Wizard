432ea7d33c689d78802f365ca79e99aeaa83da10
Dung <dungsd2005@gmail.com>
2024-10-02 16:02:35 +0700

    Added

--- src/main/CollisionCheck.java
+++ src/main/CollisionCheck.java
@@ -10,9 +10,5 @@ public class CollisionCheck {
     }
 
     public void checkTile(Entity entity) {
-        int entityLX = entity.worldX + entity.rect.x;
-        int entityRX = entity.worldX + entity.rect.x + entity.rect.width;
-        int entityUY = entity.worldY + entity.rect.y;
-        int entityDY = entity.worldY + entity.rect.y + entity.rect.height;
     }
 }
